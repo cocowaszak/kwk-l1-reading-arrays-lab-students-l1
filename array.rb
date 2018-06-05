c3af059c5c008b72c062fbcbd2ba30a6f4e96360
cocowaszak <colettewaszak@gmail.com>
2018-06-05 22:53:51 +0000

    automatically backed up by learn

---
+++ array.rb
@@ -0,0 +1,27 @@
+student_names = []
+#arrays are formed by using square brackets [ ]
+
+student_names = ['sam' , 'jess', 'ash', 'bob', 'carla']
+#puts student_names
+
+#puts [name of your array] prints out the information in your array in the terminal 
+
+#puts student_names[0]
+
+# position zero gives you the first value in the array 
+
+student_names.push('cardi')
+#puts student_names
+
+#array name ('what you want to add').push will add it to the array
+student_names.push('coco', 'juliet')
+#puts student_names
+
+
+student_names << "mac"
+# student_names
+
+# << only allows you to add one thing at a time
+student_names.push('betty', 'mack')
+puts student_names.size 
+
